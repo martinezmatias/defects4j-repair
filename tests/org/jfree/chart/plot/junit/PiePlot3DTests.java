31d2df40ea8a767b334fbbab22a3321e5d12be71
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-11-10 17:22:05 +0100

    refactor test from Chart 15 in order to throw an exception

--- tests/org/jfree/chart/plot/junit/PiePlot3DTests.java
+++ tests/org/jfree/chart/plot/junit/PiePlot3DTests.java
@@ -133,22 +133,16 @@ public class PiePlot3DTests extends TestCase {
     /**
      * Draws a pie chart where the label generator returns null.
      */
-    public void testDrawWithNullDataset() {
+    public void testDrawWithNullDataset() throws Exception {
         JFreeChart chart = ChartFactory.createPieChart3D("Test", null, true, 
                 false, false);
         boolean success = false;
-        try {
-            BufferedImage image = new BufferedImage(200 , 100, 
+
+        BufferedImage image = new BufferedImage(200 , 100, 
                     BufferedImage.TYPE_INT_RGB);
-            Graphics2D g2 = image.createGraphics();
+        Graphics2D g2 = image.createGraphics();
             chart.draw(g2, new Rectangle2D.Double(0, 0, 200, 100), null, null);
-            g2.dispose();
-            success = true;
-        }
-        catch (Exception e) {
-            success = false;
-        }
-        assertTrue(success);
+        g2.dispose();
     }
 
 }
